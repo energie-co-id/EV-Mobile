1af3dc13483233d0c9f28905ac6dc9c088cba39d
foldadjo <adibmaknun16@gmail.com>
2022-10-12 15:14:31 +0700

    add tailwind tw

---
+++ src/navigation/app.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome from "react-native-vector-icons/MaterialIcons";
+
+const Stack = createNativeStackNavigator();
+
+import Home from '../screen/Home';
+import History from '../screen/History';
+import Map from '../screen/Map';
+import TabContent from '../component/TabContent'
+
+const Tab = createBottomTabNavigator();
+
+function HomeStackScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        component={Home}
+        name="HomeStack"
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function MapStackScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        component={Map}
+        name="MapStack"
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function HistoryStackScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        component={History}
+        name="HistoryStack"
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function AppNavigator() {
+  return (
+    <Tab.Navigator
+      tabBar={props => <TabContent {...props} />}
+      screenOptions={{
+        headerShown:false
+      }}>
+      <Tab.Screen 
+        name="Home" 
+        component={HomeStackScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color, focused}) => (
+            <FontAwesome name='home' size={focused ? 30 : 25} color={color} />
+          )
+        }}
+      />
+      <Tab.Screen 
+        name="Map" 
+        component={MapStackScreen}
+        options={{
+          tabBarLabel: 'Find Station',
+          tabBarIcon: ({color, focused}) => (
+            <FontAwesome name='ev-station' size={focused ? 30 : 25} color={color} />
+          )
+        }}
+      />
+      <Tab.Screen 
+        name="History" 
+        component={HistoryStackScreen}
+        options={{
+          tabBarLabel: 'History',
+          tabBarIcon: ({color, focused}) => (
+            <FontAwesome name='history-edu' size={focused ? 30 : 25} color={color} />
+          )
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default AppNavigator;
---
+++ src/navigation/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+import SpleshScreen from '../screen/SpleshScreen';
+import Profile from '../screen/Profile';
+import AuthScreen from './auth';
+import AppScreen from './app';
+
+function MyStackNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SpleshScreen">
+        <Stack.Screen
+          component={SpleshScreen}
+          name="SpleshScreen"
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          component={AuthScreen}
+          name="AuthScreen"
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          component={AppScreen}
+          name="AppScreen"
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          component={Profile}
+          name="Profile"
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MyStackNavigator;
