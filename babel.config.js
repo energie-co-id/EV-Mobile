eb4e339549b57e3e61fca81e2aebdb90e2476c83
foldadjo <adibmaknun16@gmail.com>
2022-10-12 09:25:30 +0700

    add tailwind

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
+/* eslint-disable prettier/prettier */
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    "nativewind/babel",
+    { tailwindConfig: "./tailwind.native.config.js" },
+  ],
 };
