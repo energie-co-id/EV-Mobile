a5a05ae495964381f3f20c844e8af7964cb2bac8
Sythe007 <mrtam7777@gmail.com>
2022-10-14 22:36:43 +0700

    temp-dashboard

--- src/component/main/index.js
+++ src/component/main/index.js
@@ -12,21 +12,15 @@ function Main (props) {
   };
   return (
     <View style={[tw`h-140 bg-white`]}>
-     <View style={[tw`border-2 border-gray-300 rounded-xl bg-white mt-5 mr-5 ml-7 mb-1`]}>
-      <Text style={[tw`text-gray-500 text-xl font-bold mt-2 ml-9`]}>Konsumsi Energi Anda</Text>
-      <View style={[tw` flex flex-row`]}>
-       <Text style={[tw`text-gray-500 text-lg ml-10`]}>1</Text>
-       <Text style={[tw`text-gray-500 text-lg ml-10`]}>2</Text>
-      <TouchableOpacity><Image style={[tw`h-20 w-20 ml-11`]} source={require('../../assets/charge.png')}/></TouchableOpacity>
+     <View style={[tw`border-2 border-gray-300 rounded-xl bg-white mt-5 mr-5 ml-5 mb-2`]}>
+      <Text style={[tw`text-gray-500 text-xl font-bold mt-2 ml-5`]}>Konsumsi Energi Anda</Text>
+      {/* <View style={[tw`flex flex-row`]}> */}
+      {/* <TouchableOpacity><Image style={[tw`h-15 w-15`]} source={require('../../assets/charge.png')}/></TouchableOpacity> */}
+      {/* </View> */}
       </View>
-    
-      </View>
-    
      
-     <Text style={[tw`text-gray-500 text-lg font-bold ml-7 mb-1`]}>Konsumsi Energi Tahun ini</Text>
-     <Text style={[tw`text-gray-500 text-lg ml-7 mb-9`]}>hjshddjsdjhdsjhsj</Text>
-     <Text style={[tw`text-gray-500 text-lg ml-7 `]}>Konsumsi Energi Tahun ini</Text>
-     <TouchableOpacity style={[tw`bg-gray-200 h-65 w-79 mt-5 ml-5`]}></TouchableOpacity>
+     <Text style={[tw`text-gray-500 text-2xl font-bold mt-1 ml-7 mb-1`]}>Konsumsi Energi Tahun ini</Text>
+     <TouchableOpacity style={[tw`bg-gray-200 h-55 w-79 mt-2 ml-5`]}></TouchableOpacity>
     </View>
   );
 }
