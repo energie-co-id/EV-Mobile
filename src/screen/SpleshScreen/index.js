aeaaab8022754c2f04f494b02b59d6215a62b3ac
Sythe007 <mrtam7777@gmail.com>
2022-10-14 09:31:33 +0700

    slicing-done

--- src/screen/SpleshScreen/index.js
+++ src/screen/SpleshScreen/index.js
@@ -30,14 +30,14 @@ function SpleshSreen(props) {
       <ImageBackground source={image} resizeMode="cover" style={[tw`flex-1 justify-center items-center`]}>
         <View style={[tw`flex-row`]}>
           <View>
-            <Text style={[tw`text-black text-4xl mt-25 ml-3 font-mono`]}> Logo </Text>
+            <Text style={[tw`text-black text-5xl mt-25 ml-3 font-mono`]}> Logo </Text>
           </View>
           <View>
-            <Image source={require('../../assets/logo.png')} style={tw`w-20 h-20 p-5 mt-17`}/>
+            <Image source={require('../../assets/logo.png')} style={tw`w-20 h-20 p-5 mt-19`}/>
           </View>
         </View>
         <View>
-          <Text style={[tw`text-black text-2xl m-15 font-mono`]}> Welcome ^_^ </Text>
+          <Text style={[tw`text-black text-3xl m-11 font-mono`]}> Welcome ^_^ </Text>
         </View>
       </ImageBackground>
     </View>
