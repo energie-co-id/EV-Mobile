629af887e27307b2d8c31d5323bd79322488e68c
Sythe007 <mrtam7777@gmail.com>
2022-10-15 11:09:07 +0700

    revision-home

--- src/screen/Home/index.js
+++ src/screen/Home/index.js
@@ -31,21 +31,35 @@ function Home(props) {
       <Text style={[tw `text-gray-500 text-2xl ml-7 mt-1.1 font-mono font-bold`]}> EV Charging Hyundai </Text>
       <Text style={[tw `text-gray-500 text-sm ml-7 mb-5`]}> New Thinking, New Posibilities </Text>
 
-    <View style={[tw`h-140 bg-white`]}>
-      <View style={[tw`border-2 border-gray-300 rounded-xl bg-white mt-5 mr-5 ml-5 mb-2`]}>
+    <View style={[tw`h-170 bg-white`]}>
+      <View style={[tw`border-2 border-gray-200 rounded-xl bg-white mt-5 mr-5 ml-5 mb-2`]}>
         <Text style={[tw`text-gray-500 text-2xl font-bold mt-2 ml-5`]}>Konsumsi Energi Anda</Text>
-      <View style={[tw`flex flex-row`]}>
-        <Text style={[tw`text-gray-500 text-xl mt-2 ml-5`]}>● Hari Ini{"\n"}
-        <Text style={[tw`text-gray-500 text-xl font-bold`]}>     5.4 </Text><Text style={[tw`font-normal text-lg`]}>KwH</Text></Text>
-        <Text style={[tw`text-gray-500 text-xl mt-2 ml-5`]}>● Bulan Ini{"\n"}
-        <Text style={[tw`text-gray-500 text-xl font-bold`]}>     40.7 </Text><Text style={[tw`font-normal text-lg`]}>KwH</Text></Text>
-      <TouchableOpacity><Image style={[tw`h-20 w-20 ml-4.1 mb-3.5 justify-center items-center`]} source={require('../../assets/charge.png')}/></TouchableOpacity>
-      </View>
+        <View style={[tw`flex flex-row`]}>
+        <View style={[tw`bg-white mt-5 mb-2 w-1/3`]}>
+        <Text style={[tw`text-gray-500 text-2xl font-bold mt-5 ml-5`]}>Hari Ini</Text>
+        <Text style={[tw`text-gray-500 text-xl font-bold`]}>      5.4 KwH </Text> 
+        </View>
+        <View style={[tw`bg-white mt-5 mb-2 w-1/3`]}>
+        <Text style={[tw`text-gray-500 text-2xl font-bold mt-5 ml-2.1`]}>Bulan Ini</Text>
+        <Text style={[tw`text-gray-500 text-xl font-bold`]}>    40.7 KwH</Text>
+        </View>
+        <View style={[tw`border-l-2 border-gray-300 bg-white mt-5 mb-2 ml-0.11 w-1/3`]}>
+        <TouchableOpacity><Image style={[tw`h-20 w-20 ml-3 mb-1 justify-center items-center`]} source={require('../../assets/charge.png')}/></TouchableOpacity> 
+        <Text style={[tw`text-gray-500 text-2xl font-bold mt-1 ml-4.7 mb-3`]}>Charge</Text>
+        </View>
+        </View>
+      {/* <View style={[tw`flex flex-row`]}> */}
+        {/* <Text style={[tw`text-gray-500 text-xl mt-2 ml-5`]}>● Hari Ini{"\n"} */}
+        {/* <Text style={[tw`text-gray-500 text-xl font-bold`]}>     5.4 </Text><Text style={[tw`font-normal text-lg`]}>KwH</Text></Text> */}
+        {/* <Text style={[tw`text-gray-500 text-xl mt-2 ml-5`]}>● Bulan Ini{"\n"} */}
+        {/* <Text style={[tw`text-gray-500 text-xl font-bold`]}>     40.7 </Text><Text style={[tw`font-normal text-lg`]}>KwH</Text></Text> */}
+      {/* <TouchableOpacity><Image style={[tw`h-20 w-20 ml-4.1 mb-3.5 justify-center items-center`]} source={require('../../assets/charge.png')}/></TouchableOpacity> */}
+      {/* </View> */}
       </View>
       <Text style={[tw`text-gray-500 text-2xl font-bold mt-1 ml-7`]}>Konsumsi Energi Tahun ini</Text>
       <Text style={[tw`text-gray-500 text-lg mt-1 ml-7 mb-15`]}>Hemat pengeluaran Anda dengan monitor konsumsi energi tiap kali </Text>
       <Text style={[tw`text-gray-500 text-lg mt-1 ml-7`]}>Anda Charging</Text>
-     <TouchableOpacity style={[tw`bg-gray-200 h-47 w-79 mt-2 ml-5`]}></TouchableOpacity>
+     <TouchableOpacity style={[tw`bg-gray-200 h-59 w-79 mt-2 ml-5`]}></TouchableOpacity>
     </View>
     </ScrollView>
     
