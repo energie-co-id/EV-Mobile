9752bc6b1909cdfcd03807b4dd549b9a1c459d85
Sythe007 <mrtam7777@gmail.com>
2022-10-17 11:38:28 +0700

    location-details

--- src/screen/Map/index.js
+++ src/screen/Map/index.js
@@ -27,39 +27,44 @@ function Map(props) {
         <TouchableOpacity onPress={() => props.navigation.navigate("Profile")}><Image source={require('../../assets/logo.png')}
          style={[tw`w-20 h-20 ml-11 mt-5 mb-5`]}/></TouchableOpacity>
       </View> 
-      <View style={[tw`h-265 bg-white`]}>
+      <View style={[tw`h-295 bg-white`]}>
       <View style={[tw`flex flex-cols`]}>
 
-        <View style={[tw`rounded-xl bg-gray-100 mt-5 ml-5 mb-1 w-81 h-45`]}>
+        <View style={[tw`rounded-xl bg-gray-100 mt-5 ml-5 mb-1 w-81 h-51`]}>
           <Text style={[tw`text-gray-500 text-2xl font-bold bg-gray-100 mt-2 ml-5`]}>Serpong, Test</Text>
+          <Text style={[tw`text-gray-500 text-sm font-normal bg-gray-100 ml-5`]}>Serpong, Test</Text>
         <View style={[tw`bg-gray-100 mb-2 ml-0.21`]}>
          <TouchableOpacity><Image style={[tw`rounded-sm w-full h-35 mt-3 mb-1 justify-center items-center`]} source={require('../../assets/test.jpg')}/></TouchableOpacity> 
         </View>
         </View>
 
-        <View style={[tw`rounded-xl bg-gray-100 mt-5 ml-5 mb-1 w-81 h-45`]}>
+        <View style={[tw`rounded-xl bg-gray-100 mt-5 ml-5 mb-1 w-81 h-51`]}>
           <Text style={[tw`text-gray-500 text-2xl font-bold bg-gray-100 mt-2 ml-5`]}>Serpong, Test</Text>
+          <Text style={[tw`text-gray-500 text-sm font-normal bg-gray-100 ml-5`]}>Serpong, Test</Text>
         <View style={[tw`bg-gray-100 mb-2 ml-0.21`]}>
          <TouchableOpacity><Image style={[tw`rounded-sm w-full h-35 mt-3 mb-1 justify-center items-center`]} source={require('../../assets/test.jpg')}/></TouchableOpacity> 
         </View>
         </View>
 
-        <View style={[tw`rounded-xl bg-gray-100 mt-5 ml-5 mb-1 w-81 h-45`]}>
+        <View style={[tw`rounded-xl bg-gray-100 mt-5 ml-5 mb-1 w-81 h-51`]}>
           <Text style={[tw`text-gray-500 text-2xl font-bold bg-gray-100 mt-2 ml-5`]}>Serpong, Test</Text>
+          <Text style={[tw`text-gray-500 text-sm font-normal bg-gray-100 ml-5`]}>Serpong, Test</Text>
         <View style={[tw`bg-gray-100 mb-2 ml-0.21`]}>
          <TouchableOpacity><Image style={[tw`rounded-sm w-full h-35 mt-3 mb-1 justify-center items-center`]} source={require('../../assets/test.jpg')}/></TouchableOpacity> 
         </View>
         </View>
 
-        <View style={[tw`rounded-xl bg-gray-100 mt-5 ml-5 mb-1 w-81 h-45`]}>
+        <View style={[tw`rounded-xl bg-gray-100 mt-5 ml-5 mb-1 w-81 h-51`]}>
           <Text style={[tw`text-gray-500 text-2xl font-bold bg-gray-100 mt-2 ml-5`]}>Serpong, Test</Text>
+          <Text style={[tw`text-gray-500 text-sm font-normal bg-gray-100 ml-5`]}>Serpong, Test</Text>
         <View style={[tw`bg-gray-100 mb-2 ml-0.21`]}>
          <TouchableOpacity><Image style={[tw`rounded-sm w-full h-35 mt-3 mb-1 justify-center items-center`]} source={require('../../assets/test.jpg')}/></TouchableOpacity> 
         </View>
         </View>
 
-        <View style={[tw`rounded-xl bg-gray-100 mt-5 ml-5 mb-1 w-81 h-45`]}>
+        <View style={[tw`rounded-xl bg-gray-100 mt-5 ml-5 mb-1 w-81 h-51`]}>
           <Text style={[tw`text-gray-500 text-2xl font-bold bg-gray-100 mt-2 ml-5`]}>Serpong, Test</Text>
+          <Text style={[tw`text-gray-500 text-sm font-normal bg-gray-100 ml-5`]}>Serpong, Test</Text>
         <View style={[tw`bg-gray-100 mb-2 ml-0.21`]}>
          <TouchableOpacity><Image style={[tw`rounded-sm w-full h-35 mt-3 mb-1 justify-center items-center`]} source={require('../../assets/test.jpg')}/></TouchableOpacity> 
         </View>
