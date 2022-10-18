f8d11270eaa9155a69a9caf0dbe9675f06a791a1
Sythe007 <mrtam7777@gmail.com>
2022-10-18 16:07:05 +0700

    profilee

--- src/screen/Profile/index.js
+++ src/screen/Profile/index.js
@@ -24,11 +24,11 @@ function Profile(props) {
   return (
     <ScrollView>
         <View style={[tw`flex flex-row bg-gray-100`]}>
-        <View style={[tw`bg-gray-100 mt-5 ml-0.21 w-1/4`]}>
-        <View style={[tw`flex flex-row bg-gray-100`]}>
-        <Text style={[tw`bg-gray-100 text-gray-700 text-lg font-semibold ml-5 mt-3`]}>Lucy</Text>
-        <TouchableOpacity onPress={() => props.navigation.navigate("Profile")}><Image style={[tw`h-5 w-5 mt-4.7 ml-1.7 justify-center items-center`]} source={require('../../assets/dropdown.png')}/></TouchableOpacity>
-        <TouchableOpacity onPress={() => props.navigation.navigate("Profile")}><Image style={[tw`h-5 w-5 mt-5.8 ml-59.5 justify-center items-center`]} source={require('../../assets/burger.png')}/></TouchableOpacity>  
+         <View style={[tw`bg-gray-100 mt-5 ml-0.21 w-1/4`]}>
+          <View style={[tw`flex flex-row bg-gray-100`]}>
+           <Text style={[tw`bg-gray-100 text-gray-700 text-lg font-semibold ml-5 mt-3`]}>Lucy</Text>
+            <TouchableOpacity onPress={() => props.navigation.navigate("Profile")}><Image style={[tw`h-5 w-5 mt-4.7 ml-1.7 justify-center items-center`]} source={require('../../assets/dropdown.png')}/></TouchableOpacity>
+           <TouchableOpacity onPress={() => props.navigation.navigate("Profile")}><Image style={[tw`h-5 w-5 mt-5.8 ml-59.5 justify-center items-center`]} source={require('../../assets/burger.png')}/></TouchableOpacity>  
         </View>
         <TouchableOpacity onPress={() => props.navigation.navigate("Profile")}><Image style={[tw`h-20 w-20 mt-5 ml-3 justify-center items-center`]} source={require('../../assets/logo.png')}/></TouchableOpacity> 
         <Text style={[tw`text-gray-700 text-xl font-semibold ml-9 mb-3`]}>Lucy</Text>
@@ -49,12 +49,12 @@ function Profile(props) {
         </View>
  
          </View>
-         <View style={[tw`bg-gray-100`]}>
-         <TouchableOpacity onPress={() => props.navigation.navigate("Profile")} style={[tw`bg-gray-200 border-2 border-gray-100 ml-5 mr-5 mb-5`]}><Text style={[tw`text-gray-700 text-center text-lg font-bold p-1`]}>Edit Profile</Text></TouchableOpacity>  
+           <View style={[tw`bg-gray-100`]}>
+           <TouchableOpacity onPress={() => props.navigation.navigate("Profile")} style={[tw`bg-gray-200 border-2 border-gray-100 ml-5 mr-5 mb-5`]}><Text style={[tw`text-gray-700 text-center text-lg font-bold p-1`]}>Edit Profile</Text></TouchableOpacity>  
          </View>
       
       <View style={[tw`h-117 bg-gray-50`]}>
-      <TouchableOpacity style={[tw`bg-gray-50 h-97 w-85`]}></TouchableOpacity>
+      <TouchableOpacity style={[tw`bg-gray-50 ml-5 h-97 w-85`]}></TouchableOpacity>
       <TouchableOpacity onPress={() => props.navigation.navigate("AppScreen", {screen: 'Home'})}><Image source={require('../../assets/back.png')} style={[tw`bg-gray-50 h-14.1 w-14.1 mt-1 ml-71 justify-center items-center`]}/></TouchableOpacity>
       
       </View>
