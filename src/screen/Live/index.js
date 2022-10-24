14d40947e3c4e279bc3795e963bad19e7d90d0e5
Sythe007 <mrtam7777@gmail.com>
2022-10-24 17:00:37 +0700

    ui-styling-icon

--- src/screen/Live/index.js
+++ src/screen/Live/index.js
@@ -32,20 +32,20 @@ const Live = (props) => {
      <View style={[tw`flex flex-row bg-white`]}>
 
      <View style={[tw`bg-white w-40 mt-3 ml-5`]}>
-        <Text style={[tw`text-gray-500 ml-12.5 text-lg font-bold`]}>Energy</Text>
-        <Icon style={[tw`rounded-xl bg-gray-200 ml-11 text-center w-20 p-1`]} name='ev-station' size={50} color={'#666666'} />
-        <Text style={[tw`text-gray-500 ml-10.5 text-lg`]}><Text style={[tw`text-gray-500 text-lg font-bold p-2`]}>10.32 </Text>KwH</Text>
+        <Text style={[tw`text-gray-700 ml-12.5 text-lg font-bold`]}>Energy</Text>
+        <Icon style={[tw`rounded-xl bg-gray-200 ml-11 text-center w-20 p-1`]} name='ev-station' size={50} color={'#333333'} />
+        <Text style={[tw`text-gray-700 ml-10.5 text-lg`]}><Text style={[tw`text-gray-500 text-lg font-bold p-2`]}>10.32 </Text>KwH</Text>
     </View>
 
     <View style={[tw`bg-white w-38 mt-3 ml-3.5`]}>
-         <Text style={[tw`text-gray-500 ml-11.5 text-lg font-bold`]}>Duration</Text>
-         <Icon style={[tw` rounded-xl bg-gray-200 ml-10.5 text-center w-20 p-1`]} name='timer' size={50} color={'#666666'} />
-         <Text style={[tw`text-gray-500 ml-9.5 text-lg font-bold`]}>00:01:59</Text>
+         <Text style={[tw`text-gray-700 ml-11.5 text-lg font-bold`]}>Duration</Text>
+         <Icon style={[tw` rounded-xl bg-gray-200 ml-10.5 text-center w-20 p-1`]} name='timer' size={50} color={'#333333'} />
+         <Text style={[tw`text-gray-700 ml-9.5 text-lg font-bold`]}>00:01:59</Text>
      </View>
      
      </View>
-     <View style={[tw` rounded-full flex flex-col bg-gray-100  h-30 w-30 ml-33 mr-5 mt-5`]}>
-     <TouchableOpacity onPress={() => props.navigation.navigate("Live")}><Icon style={[tw`ml-3 mt-3`]} name='power-settings-new' size={95} color={'#666666'} /></TouchableOpacity> 
+     <View style={[tw` rounded-full flex flex-col bg-gray-500 h-30 w-30 ml-33 mr-5 mt-5`]}>
+     <TouchableOpacity onPress={() => props.navigation.navigate("Live")}><Icon style={[tw`ml-3 mt-3`]} name='power-settings-new' size={95} color={'#ffff'} /></TouchableOpacity> 
         </View>
      
     
