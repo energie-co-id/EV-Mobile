dcf4fba2a49f5075ae5266742137c6728eb14692
Sythe007 <mrtam7777@gmail.com>
2022-10-24 13:02:21 +0700

    text-typoo

--- src/screen/History/index.js
+++ src/screen/History/index.js
@@ -23,7 +23,7 @@ function History(props) {
   return (
     <ScrollView>
       <View style={[tw`flex flex-row`]}>
-      <Text style={[tw`text-gray-500 font-normal ml-11 mt-9`]}><Text style={[tw `text-3xl text-gray-500 font-bold`]}>History</Text>{`\n`}Present our charging record</Text>
+      <Text style={[tw`text-gray-500 font-normal ml-11 mt-9`]}><Text style={[tw `text-3xl text-gray-500 font-bold`]}>History</Text>{`\n`}Present your charging record</Text>
         <TouchableOpacity onPress={() => props.navigation.navigate("Profile")}><Image source={require('../../assets/logo.png')}
          style={[tw`w-20 h-20 ml-5 mt-5 mb-5`]}/></TouchableOpacity>
       </View> 
